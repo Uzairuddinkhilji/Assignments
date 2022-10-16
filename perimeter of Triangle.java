8bb182c20304e85ab6f3bce89ea973fd893b6a25
Uzair Ud Din <115455370+Uzairuddinkhilji@users.noreply.github.com>
2022-10-16 18:57:30 +0500

    Add files via upload
    0bject Oriented Programming (OOP)
    Assignment:
    1: Area Of Triangle.
    2: Perimeter Of Triangle.

---
+++ perimeter of Triangle.java
@@ -0,0 +1,18 @@
+public class Triangle
+{
+	    int side a;
+	    int side b;
+	    int side c;
+	    
+	    Triangle(int a, int b, int c)
+	    {
+	    	this.side a=a;
+	    	this.side b=b;
+	    	this.side c=c;
+	    }
+	    int perimeter()
+	    {
+	    	return (side a+side b+side c)
+	    }
+}
+	    
